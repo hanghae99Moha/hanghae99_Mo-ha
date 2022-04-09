08d26845c4304306243dc3d83a5153e6d145b027
hsangwon <101050184+hsangwon@users.noreply.github.com>
2022-04-09 12:34:31 +0900

    페이지 라우트

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,16 +1,41 @@
 import React from "react";
 import { Grid } from "../elements";
+import { Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 // components
 import Header from "../components/Header.jsx";
 import Sidebar from "../components/Sidebar.jsx";
 
+//pages
+import Login from "../pages/Login";
+import Main from "../pages/Main";
+import Category from "../pages/Category";
+import Detail from "../pages/Detail";
+import Mypost from "../pages/Mypost";
+import Post from "../pages/Post";
+import Signup from "../pages/Signup";
+import Update from "../pages/Update";
+
 function App() {
   return (
     <div className="App">
       <Grid is_flex>
-        <Sidebar />
-        <Header />
+        <BrowserRouter>
+          <Route path="/" exact>
+            <Login />
+          </Route>
+          <Route path="/main" exact component={Main}>
+            <Sidebar />
+            <Header />
+          </Route>
+          <Route path="/category" exact component={Category} />
+          <Route path="/detail" exact component={Detail} />
+          <Route path="/mypost" exact component={Mypost} />
+          <Route path="/post" exact component={Post} />
+          <Route path="/signup" exact component={Signup} />
+          <Route path="/update" exact component={Update} />
+        </BrowserRouter>
       </Grid>
     </div>
   );
