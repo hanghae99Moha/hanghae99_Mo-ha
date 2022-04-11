e84b67665120438b0ee7800f3c8bc59868658f80
hsangwon <101050184+hsangwon@users.noreply.github.com>
2022-04-12 03:54:50 +0900

    로그인연결커밋

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,8 @@
+import axios from "axios";
 import React from "react";
 
-const apis = () => {
-  return <div>api</div>;
-};
+const api = axios.create({
+  baseURL: "http://13.125.73.243/",
+});
 
-export default apis;
+export default api;
